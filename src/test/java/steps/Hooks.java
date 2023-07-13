f7c518abbb8846328225224638c18da4e8d5730a
Alex <alexey.rashevskyi@gmail.com>
2023-07-12 22:21:54 -0400

    add the start method to the Hooks class

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,6 +1,15 @@
 package steps;
 
-public class Hooks {
+import org.junit.Before;
+import utils.CommonMethods;
+
+public class Hooks extends CommonMethods {
+    @Before
+    public void start() {
+        //browser name we should add from the openBrowserGetURL method
+        // in the CommonMethods class
+        openBrowserGetURL(getProperty("url"), "chrome");
+    }
 
 
 }
--- src/test/java/steps/PageInitializer.java
+++ src/test/java/steps/PageInitializer.java
@@ -2,8 +2,8 @@ package steps;
 
 public class PageInitializer {
 
-
-    //methods in which we will add in the future all our object of the POM classes
+    //methods in which we will add in the future all our
+    // object of the POM classes
     public static void initializePageObjects() {
 
     }
