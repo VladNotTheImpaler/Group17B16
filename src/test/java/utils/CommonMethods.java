9c9cd11e962ea986cdeb055f9c2ffd9249ec2284
ntkovch <ntkovch@gmail.com>
2023-07-13 18:18:24 +0300

    create configReader methods and waitForClick

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -1,4 +1,41 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
 public class CommonMethods {
+        public static WebDriver driver;
+        static Properties properties;
+
+        public static Properties readProperties(String filePath) {
+                try {
+                        FileInputStream fileIn = new FileInputStream(filePath);
+                        properties = new Properties();
+                        properties.load(fileIn);
+                } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+                return properties;
+        }
+
+        public static String getPropertyValue(String key){
+                return properties.getProperty(key);
+        }
+        public static WebDriverWait getWait(){
+                WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+                return  wait;
+        }
+        public static void waitForClickability(WebElement element){
+                getWait().until(ExpectedConditions.elementToBeClickable(element));
+        }
 }
