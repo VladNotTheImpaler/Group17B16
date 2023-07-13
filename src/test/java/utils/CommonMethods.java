407f7b1d185a970139f3f83b554d293854f3ef7b
LaisLatifi <mudaser1992@gmail.com>
2023-07-13 10:42:59 -0700

    I added some method of send text and close

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -1,4 +1,18 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class CommonMethods {
+    public static WebDriver driver;
+    public static void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+    public static void sendText(String text, WebElement element) {
+        element.clear();
+        element.sendKeys(text);
+    }
 }
+
