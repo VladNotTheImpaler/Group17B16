61ac6f1f481ac96dabee23a25e0135740860d169
Alex <alexey.rashevskyi@gmail.com>
2023-07-12 22:13:25 -0400

    add the method to the PageInitializer class

--- src/test/java/steps/PageInitializer.java
+++ src/test/java/steps/PageInitializer.java
@@ -5,7 +5,7 @@ public class PageInitializer {
 
     //methods in which we will add in the future all our object of the POM classes
     public static void initializePageObjects() {
-        
+
     }
 
 
