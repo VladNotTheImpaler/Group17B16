69a2343280bcf70c5dbeeac9005b557170b0be04
Alex <alexey.rashevskyi@gmail.com>
2023-07-12 22:06:58 -0400

    add the path to the config.properties in constants

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -1,6 +1,8 @@
 package utils;
 
 public class Constants {
+
+    //add the path to the config.properties file
     public static final String CONFIG_READER_PATH = System.getProperty("user.dir") +
             "/src/test/resources/config/config.properties";
 }
