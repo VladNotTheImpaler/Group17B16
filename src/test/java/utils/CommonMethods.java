ca7fa465a8fc7d1fa031d3c9ab2d25e215376ac3
Halima-Sayed <halimasayed1999@gmail.com>
2023-07-13 14:30:02 -0400

    Code had many errors all good now.

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -5,6 +5,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
@@ -18,42 +23,13 @@ public class CommonMethods {
     public static FirefoxOptions optionsFireFox;
 
 
-    //method for open browser and getting the url from the Config.properties file
-    public static void openBrowserGetURL(String url, String browser) {
-        getProperties(Constants.CONFIG_READER_PATH);
-        switch (browser) {
-            case "chrome" -> {
-                optionsChrome = new ChromeOptions();
-                // set language
-                optionsChrome.addArguments("--lang=en");
-                //  create instance
-                driver = new ChromeDriver(optionsChrome);
-            }
-            case "firefox" -> {
-                optionsFireFox = new FirefoxOptions();
-                // set language
-                optionsFireFox.addArguments("--lang=en");
-                //  create instance
-                driver = new FirefoxDriver(optionsFireFox);
-            }
-        }
-
-        driver.manage().window().maximize();
-        driver.get(url);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        //this method is going to initialize all the objects available
-        // inside this method that is located in PageInitializer class
-        initializePageObjects();
-
-    }
-
     // Method to switch driver focus to a frame using frame ID or name
     public void switchToFrameByNameOrId(String frameNameOrId) {
-        driver.switchTo.frame(frameNameOrId);
+        driver.switchTo().frame(frameNameOrId);
     }
     // Method to switch driver focus to a frame using frame Index
     public void swithToFrameByIndex(int frameIndex) {
-        driver.switchTo.frame(frameIndex);
+        driver.switchTo().frame(frameIndex);
     }
     // Method to switch driver focus to a frame using frame WebElement
     public void switchToFrameByElement(WebElement frameElement) {
@@ -64,7 +40,7 @@ public class CommonMethods {
         driver.switchTo().defaultContent();
     }
 
-    public void takeScreenshotAndSave(String fileName) {
+  /*  public void takeScreenshotAndSave(String fileName) {
         TakesScreenshot ts = (TakesScreenshot) driver;
         byte[] picBytes = ts.getScreenshotAs(OutputType.BYTES);
         File screenShot = ts.getScreenshotAs(OutputType.FILE);
@@ -76,8 +52,9 @@ public class CommonMethods {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+
+   */
 }
 
 
